b55cd0ac21a25f38cc24e11a068aa6e21d277434
ViniciusSantos19 <vinicontatosq@gmail.com>
2024-08-27 18:13:17 -0300

    camada rest do projeto

---
+++ src/main/java/com/example/demo/adapter/api/CommandDto.java
@@ -0,0 +1,5 @@
+package com.example.demo.adapter.api;
+
+public record CommandDto(String command) {
+
+}
---
+++ src/main/java/com/example/demo/adapter/api/SqlCommandController.java
@@ -0,0 +1,31 @@
+package com.example.demo.adapter.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.application.usecase.SendSqlCommandUseCase;
+import com.example.demo.domain.Command;
+
+@RestController
+
+@RequestMapping("/command")
+public class SqlCommandController {
+
+	
+	@Autowired
+	private SendSqlCommandUseCase sendSqlCommandUseCase;
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public void enviarComando(@RequestBody CommandDto comando) {
+		System.out.println("Received SQL Command on the controller: " + comando);
+		var command = new Command(comando.command());
+		sendSqlCommandUseCase.executeCommand(command);
+	}
+	
+}
