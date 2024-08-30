de5fdbec778e7cfc7b08231e3990f9f22c6306df
ViniciusSantos19 <vinicontatosq@gmail.com>
2024-08-30 16:06:12 -0300

    removi espaço que estava bugando o log

--- src/main/java/com/example/demo/application/service/CheckIfReplicaIsAliveService.java
+++ src/main/java/com/example/demo/application/service/CheckIfReplicaIsAliveService.java
@@ -10,36 +10,34 @@ import com.example.demo.application.port.queue.FailsNotfyMessageProducerPort;
 import com.example.demo.application.port.queue.remove.RemoveBindPort;
 import com.example.demo.application.usecase.CheckIfReplicaIsAliveUseCase;
 
-public class CheckIfReplicaIsAliveService implements CheckIfReplicaIsAliveUseCase{
+public class CheckIfReplicaIsAliveService implements CheckIfReplicaIsAliveUseCase {
 
-	private final Map<String, Long> replicaStatus = new ConcurrentHashMap<>();
-	
-	@Autowired
-	private FailsNotfyMessageProducerPort failsNotfyMessageProducerPort;
-	
-	@Autowired
-	private RemoveBindPort removeBindPort;
-	
-	@Override
-	public void updateReplicaStatus(String replicaId, Long miliseconds) {
-		replicaStatus.put(replicaId, System.currentTimeMillis());
-	}
+  private final Map<String, Long> replicaStatus = new ConcurrentHashMap<>();
 
+  @Autowired
+  private FailsNotfyMessageProducerPort failsNotfyMessageProducerPort;
 
+  @Autowired
+  private RemoveBindPort removeBindPort;
 
-	@Scheduled(fixedRate = 50000)
-	@Override
-	public void cleanupExpiredReplicas() {
-		 long currentTime = System.currentTimeMillis();
-	        replicaStatus.entrySet().removeIf(entry -> {
-	        	if ((currentTime - entry.getValue()) > 60000) {
-	        		removeBindPort.unbindQueue("instancia-"+entry.getKey());
-	        		removeBindPort.unbindQueue("notfy-instancia-"+entry.getKey());
-	        		failsNotfyMessageProducerPort.notfyFail(entry.getKey());
-	        		System.out.println("A replica de id:"+ entry.getKey()+ "foi removida pelo lider");
-	                return true;
-	            }
-	        	return false;
-	        });
-	}
+  @Override
+  public void updateReplicaStatus(String replicaId, Long miliseconds) {
+    replicaStatus.put(replicaId, System.currentTimeMillis());
+  }
+
+  @Scheduled(fixedRate = 50000)
+  @Override
+  public void cleanupExpiredReplicas() {
+    long currentTime = System.currentTimeMillis();
+    replicaStatus.entrySet().removeIf(entry -> {
+      if ((currentTime - entry.getValue()) > 60000) {
+        removeBindPort.unbindQueue("instancia-" + entry.getKey());
+        removeBindPort.unbindQueue("notfy-instancia-" + entry.getKey());
+        failsNotfyMessageProducerPort.notfyFail(entry.getKey());
+        System.out.println("A replica de id:" + entry.getKey() + " foi removida pelo lider");
+        return true;
+      }
+      return false;
+    });
+  }
 }
