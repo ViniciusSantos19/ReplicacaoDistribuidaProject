4e19d388c5362c8f703ceb5c05c462e6510ef852
ViniciusSantos19 <vinicontatosq@gmail.com>
2024-09-03 14:50:10 -0300

    adicionei comando para imprimir tabela terminal

--- src/main/java/com/example/demo/adapter/queue/SqlCommandConsumerAdapter.java
+++ src/main/java/com/example/demo/adapter/queue/SqlCommandConsumerAdapter.java
@@ -1,5 +1,8 @@
 package com.example.demo.adapter.queue;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -22,9 +25,19 @@ public class SqlCommandConsumerAdapter implements SqlCommandConsumerPort{
 	private void processarComandoSql(String message) {
 		System.out.println("Received SQL Command: " + message);
 		try {
+			if (message.trim().toUpperCase().startsWith("SELECT")) {
+				List<Map<String, Object>> results = jdbcTemplate.queryForList(message);
+
+	            // Print the results
+	            for (Map<String, Object> row : results) {
+	                System.out.println(row);
+	            }
+	            return;
+			}
+			
 			jdbcTemplate.execute(message);
 		} catch (Exception e) {
-			e.getStackTrace();
+			e.printStackTrace();;
 		}
 	}
 
